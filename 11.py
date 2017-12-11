a77248690e4ea0f5fc0c16806c5b06eaf13addd9
Rui Maranhao <rui@dashdash.com>
2017-12-11 10:59:19 +0000

    update

--- 11.py
+++ 11.py
@@ -3,11 +3,7 @@ INCS = {"n":  (1,0,-1),
         "se": (-1,1,0),
         "s":  (-1,0,1),
         "sw": (0,-1,1),
-        "nw": (1,-1,0) }
-
-INCS_2D = {'n': (0, 1), 'ne': (1, 1), 'nw': (-1, 1),
-           's': (0, -1), 'se': (1, -1), 'sw': (-1, -1),
-           'e': (1, 0), 'w': (-1, 0)}
+        "nw": (1,-1,0)}
 
 
 def read_path(name):
@@ -30,24 +26,7 @@ def far(path):
     return __far(curr), furthest
 
 
-def far2D(path):
-    curr = (0, 0)
-    last = ''
-    for p in path:
-        now = p
-        if (last == 'sw' and now == 'se') or (last == 'nw' and now == 'ne'):
-            now = 'e'
-        elif (last == 'se' and now == 'sw') or (last == 'ne' and now == 'nw'):
-            now = 'w'
-        (x, y) = INCS_2D[now]
-        curr = (curr[0] + x, curr[1] + y)
-        last = now
-
-    return max(abs(curr[0]), abs(curr[1]))
-
-
 if __name__ == '__main__':
     paths = read_path('11.txt')
     for p in paths:
         print(far(p))
-        print("\t-->", far2D(p))
